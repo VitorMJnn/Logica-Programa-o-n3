920b9309617e991da243ec37d4b6217db33d988d
VITOR MANUEL JOAQUIM NUNES LIMA <vitor.mjnlima@senacsp.edu.br>
2025-11-03 21:25:20 -0300

    primeiro commit com codigo

---
+++ Aula 3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1");
+Console.WriteLine("2");
+
+int x = 1;
+int y= 2;
+
+double resutadoA = (x + y);
+Console.WriteLine(resutadoA);
+
+double resutadoB = (x - y);
+Console.WriteLine(resutadoB);
+
+double resutadoC = (x * y);
+Console.WriteLine(resutadoC);
+
+double resutadoD = (x / y);
+
+int n1;
+int n2;
+
+
+
+
+
+
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+double x = 1;
+int y = 1;
+int z = 2;
+
+double resutado = ((x + y) * z);
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(resutado);
+
+Console.WriteLine("\n----\n");
+
+string nome = "vitor";
+string sobrenome = "lima";
+Console.WriteLine(nome + sobrenome);
+
+char lestraA = '0';
+char letrasB = '1';
+Console.WriteLine(lestraA +0);
+Console.WriteLine(letrasB +0);
+
+Console.WriteLine("\n----\n");
+
+Console.WriteLine("EScreva seu nome");
+string nomeUsuario = Console.ReadLine();
+Console.WriteLine("O seu nome é:" +  nomeUsuario);
+
+Console.WriteLine("Em qual ano vc nasceu?");
+string anoNascimentoidadeUsuarioString= Console.ReadLine();
+Console.WriteLine("A sua idade é: + anoNascimentoidadeUsuarioString");
+int idadeUsuario = 2025 - int.Parse(anoNascimentoidadeUsuarioString);
+Console.WriteLine("A sua iadae é:" + idadeUsuario);
