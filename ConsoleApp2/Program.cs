cf90380c422e443e242866e21df66aaee6d88c76
VITOR MANUEL JOAQUIM NUNES LIMA <vitor.mjnlima@senacsp.edu.br>
2025-11-10 19:32:01 -0300

    primeiro commit com codigo

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//int n1;
+//int n2;
+
+
+//Console.WriteLine("insira um numero inteiro");
+//n1 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("insira mais um numero intero");
+//n2 = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("voce digitou " + n1 + " e " + n2);
+int n1;
+int n2;
+
+Console.WriteLine("digite um numero");
+n1 = + int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite um numero");
+n2 = + int.Parse(Console.ReadLine());
+git
+Console.WriteLine("O valor da soma eh: " + (soma));
+
+Console.WriteLine("========");
+
+
+int soma = +n1 + n2;
+int sub = -n1 - n2;
+
+//int n3;
+//int n4;
+
+//Console.WriteLine("digite um numero");
+//n3 = +int.Parse(Console.ReadLine());
+
+//Console.WriteLine("digite um numero");
+//n4 = +int.Parse(Console.ReadLine());
+
+//Console.WriteLine("O valor da  SUBITRAÇAO eh: " + (n3 - n4));
