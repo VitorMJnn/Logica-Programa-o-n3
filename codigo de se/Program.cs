8aff0ad4431ef0518202ed3560522c69fa3fc347
VITOR MANUEL JOAQUIM NUNES LIMA <vitor.mjnlima@senacsp.edu.br>
2025-11-12 19:07:00 -0300

    primeiro commit com codigo

---
+++ codigo de se/Program.cs
@@ -0,0 +1,31 @@
+﻿const int DEZOITO = 18;
+const int ZERO = 0;
+
+// 
+
+Console.WriteLine("Qual e seua idade");
+
+string resposta = Console.ReadLine();
+
+bool enValido = int.TryParse(resposta, out int idade );
+
+if ( enValido )
+
+
+if (idade == DEZOITO)
+{
+    Console.WriteLine("usuario e maior de idade");
+
+}
+else if(idade  <ZERO)
+{
+    Console.WriteLine("idade invalida");
+}
+else
+{
+    Console.WriteLine("USUARIO MENOR DE IDADE");
+}
+else
+{
+    Console.WriteLine("Valor informado nao correponde a numero intero");
+}    
